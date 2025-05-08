8e098950c9f1c1f8c46c60233452dd894c4f6c8c
Abraham Flowers <svladimirov91@gmail.com>
2025-05-08 12:26:17 +0300

    added 1 more test

--- categories/tests/test_models.py
+++ categories/tests/test_models.py
@@ -10,9 +10,9 @@ User = get_user_model()
 @pytest.mark.django_db
 def test_category_creation_sets_slug_and_timestamps():
     user = User.objects.create_user(username='testuser', password='psw123')
-    category = Category.objects.create(name='Тестовая категория', owner=user)
+    category = Category.objects.create(name='Категория1', owner=user)
 
-    assert category.slug == slugify('Тестовая категория')
+    assert category.slug == slugify('Категория1')
     assert category.created_at is not None
     assert category.updated_at is not None
 
@@ -20,15 +20,26 @@ def test_category_creation_sets_slug_and_timestamps():
 @pytest.mark.django_db
 def test_category_str_returns_name():
     user = User.objects.create_user(username='anotheruser', password='qwerty')
-    category = Category.objects.create(name='Категория', owner=user)
-    assert str(category) == 'Категория'
+    category = Category.objects.create(name='Категория2', owner=user)
+    assert str(category) == 'Категория2'
 
 
 @pytest.mark.django_db
 def test_category_unique_name():
     user = User.objects.create_user(username='owner', password='pass')
-    Category.objects.create(name='Уникальная', owner=user)
+    name = 'Категория3'
+    Category.objects.create(name=name, owner=user)
 
     with pytest.raises(IntegrityError):
         # Пробуем создать дубликат
-        Category.objects.create(name='Уникальная', owner=user)
+        Category.objects.create(name=name, owner=user)
+
+
+@pytest.mark.django_db
+def test_category_unique_slug():
+    user = User.objects.create_user(username='slugger', password='3210')
+    Category.objects.create(name='Категория4', owner=user)
+
+    with pytest.raises(IntegrityError):
+        # Пробуем создать дубликат
+        Category.objects.create(name='Категория4', owner=user)
